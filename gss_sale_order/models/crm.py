a337dc8d2f937cd4368043dd3aa5435f35bab9f6
Emmanuel MOUSSI <moussipeee@gmail.com>
2023-04-12 20:31:04 +0100

    sale order

--- gss_sale_order/models/crm.py
+++ gss_sale_order/models/crm.py
@@ -25,33 +25,33 @@ class gss_sale_crmlead(models.Model):
         for line in self.vendor_ids:
             data = {'name': line.name.id,
                     'product_tmpl_id': line.product_tmpl_id.id,
-                    'price': line.price,
+                    'price': line.price or line.price_usd,
                     'min_qty': line.min_qty,
                     'delay': line.delay} 
         
             self.env['product.supplierinfo']._check_vendors(data)  
-            if line.vendor_select:
+            if line.vendor_select and self.env['product.supplierinfo']._check_vendors(data):
                 product = self.env['product.product'].search(
                     [('product_tmpl_id','=',line.product_tmpl_id.id)],
                     limit=1)
-                vendor = self.env['product.supplierinfo'].search(
-                    [('name','=',line.name.id),('product_tmpl_id','=',line.product_tmpl_id.id),
-                     ('price','=',line.price),('min_qty','=',line.min_qty),('delay','=', line.delay)],limit=1)
+                # vendor = self.env['product.supplierinfo'].search(
+                #     [('name','=',line.name.id),('product_tmpl_id','=',line.product_tmpl_id.id),
+                #      ('price','=',line.price),('min_qty','=',line.min_qty),('delay','=', line.delay)],limit=1)
                   
-                price = line.price
+                # price = line.price
                 discount = 0
                 vals ={
                     'display_type': False,
                     'name': product.name,
                     'state': 'draft',
                     'price_usd': line.price_usd,
-                    'price_unit': price,
+                    'price_unit_cad':  line.price,
                     'discount': discount,
                     'product_uom_qty': line.min_qty,
                     'product_id': product.id,
                     'product_uom': line.product_uom.id,
                     'order_id': sale_id.id,
-                    'vendor_id': vendor.id,
+                    'vendor_id': self.env['product.supplierinfo']._check_vendors(data),
                 }
               
                 order_lines.create(vals)
